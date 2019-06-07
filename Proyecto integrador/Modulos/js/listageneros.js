88c88da68b0b57a9b1601ceee23420127c24185c
Joaquin Berardi <joaquinberardi@gmailcom>
2019-06-07 18:16:50 -0300

    backup

--- Proyecto integrador/Modulos/js/listageneros.js
+++ Proyecto integrador/Modulos/js/listageneros.js
@@ -3,7 +3,7 @@
 ////////////////////////// CARGA listadoGeneros //////////////////////////////////////////////////
 
 // <a class="dropdown-item" href="#">Acción</a>
-alert("hola")
+
 fetch("https://api.themoviedb.org/3/genre/movie/list?api_key=0bcd16440b25702a4e2645e9b22f2a2d&language=en-US")
   .then(function(respuesta) {
     return respuesta.json()
@@ -15,13 +15,14 @@ fetch("https://api.themoviedb.org/3/genre/movie/list?api_key=0bcd16440b25702a4e2
     var nombre
     for (var i = 0; i < informacion.length; i++) {
         console.log(informacion[i]);
-        imagen =  "https://image.tmdb.org/t/p/w500/" + informacion.results[i].poster_path;
-        nombre = informacion.results[i].title;
-        div = "<div class='col-md-6 populares'>"
+        document.querySelector("div.dropdown-menu div.listaGeneros")
+/*        div = "<div class='col-md-6 populares'>"
         div +=    "<h3>" + nombre + "<h3>";
         div +=    "<img class='w-100' src=" + imagen + ">"
         div += "</div>"
         document.querySelector("div.nuevosLanzamientos").innerHTML += div
+*/
+// Tengo que cambiar el div por un ul con li internos extraidos de la api.
 
     }
   })
