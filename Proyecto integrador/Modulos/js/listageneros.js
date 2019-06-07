1c28f9327bdfe6adb01ea58fc9039bf7aec9b8f1
Joaquin Berardi <joaquinberardi@gmailcom>
2019-06-07 18:52:29 -0300

    Lista generos lista

--- Proyecto integrador/Modulos/js/listageneros.js
+++ Proyecto integrador/Modulos/js/listageneros.js
@@ -9,22 +9,21 @@ fetch("https://api.themoviedb.org/3/genre/movie/list?api_key=0bcd16440b25702a4e2
     return respuesta.json()
   })
   .then(function(informacion) {
-    console.log(informacion);
-    console.log(informacion.length);
+    console.log(informacion)
     var div
     var nombre
-    for (var i = 0; i < informacion.length; i++) {
-        console.log(informacion[i]);
-        document.querySelector("div.dropdown-menu div.listaGeneros")
-/*        div = "<div class='col-md-6 populares'>"
-        div +=    "<h3>" + nombre + "<h3>";
-        div +=    "<img class='w-100' src=" + imagen + ">"
-        div += "</div>"
-        document.querySelector("div.nuevosLanzamientos").innerHTML += div
-*/
+    var lista = document.querySelector("#generos");
+console.log(lista);
+    for (var i = 0; i < informacion.genres.length; i++) {
+        // console.log(informacion.genres[i]);
+        nombre = informacion.genres[i].name;
+        console.log(nombre);
+        lista.innerHTML += "<a class='dropdown-item' href='#'>" + nombre + "</a>";
+        // console.log(lista);
+
+        }
 // Tengo que cambiar el div por un ul con li internos extraidos de la api.
 
-    }
   })
   .catch(function(error) {
     console.log("Error: " + error);
