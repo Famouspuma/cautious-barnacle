59547809f701d1c080d8f989ab4ed7619a4bc348
Unknown <mdoman@udesa.edu.ar>
2019-06-14 17:02:43 -0300

    Log in avanzado

--- Proyecto integrador/Modulos/js/WebStorage.js
+++ Proyecto integrador/Modulos/js/WebStorage.js
@@ -1,33 +1,74 @@
 window.addEventListener("load", function () {
 
+  var baseDatos = JSON.parse(window.localStorage.getItem('user'))
+  if (baseDatos.nombre != "") {
+    UIkit.notification({
+        message: 'Bienvenido '+ baseDatos.nombre,
+        status: 'primary',
+        pos: 'bottom-center',
+        timeout: 5000
+    });  }
+
+
 var form = document.querySelector("#form")
 // var theForm= form.elements;
 // var arrayForm= Array.from(theForm);
 // arrayForm.pop();
 
 
-form.onsubmit= function () {
-      // 1) validamos la data que contiene cada camppo
-      var name = ""
-      var email = ""
-      var genre = ""
-      //2) en el caso que pase todas las validaciones, guardo la data del usuario
+form.onsubmit= function (event) {
+      var inputNombre = document.querySelector("#name")
+      var inputEmail = document.querySelector("#email")
+      var inputGenre = document.querySelector("#genre")
+      var select = inputGenre.options[ inputGenre.selectedIndex].text
+      var emailRegex = "/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/"
 
-      var Usuario = {
-        nombre: name,
-        correo: email,
-        genero: genre
-      }
+      // 1) validamos la data que contiene cada camppo
+      if (inputNombre.value.length < 3) {
+        event.preventDefault();
+        UIkit.notification({
+            message: 'Ingresa un nombre mayor de 3 caracteres o mas',
+            status: 'warning',
+            pos: 'bottom-center',
+            timeout: 5000
+        });
+      }else if (inputEmail.value == "" ) {
+        event.preventDefault();
+        UIkit.notification({
+            message: 'Ingresa un email valido',
+            status: 'primary',
+            pos: 'bottom-center',
+            timeout: 5000
+        });
+
+      }else if (select == "Elegir...") {
+        event.preventDefault()
+        UIkit.notification({
+            message: 'Elegi un genero',
+            status: 'primary',
+            pos: 'bottom-center',
+            timeout: 5000
+        });
+
+      }else {
+        var name = inputNombre.value
+        var email = inputEmail.value
+        var genre = select
+
+        var Usuario = {
+          nombre: name,
+          correo: email,
+          genero: genre
+        }
 
         window.localStorage.setItem('user', JSON.stringify(Usuario))
-        console.log(window.localStorage.getItem('user x'))
+        console.log(window.localStorage.getItem('user'))
         console.log(JSON.parse(window.localStorage.getItem('user')))
+      }
+      //2) en el caso que pase todas las validaciones, guardo la data del usuario
 
 
-       if (form.value=="") {
-         event.preventDefault();
-         alert ("Debes llenar los campos")
-       }
+
 
 
       //3) modifico el HTML para que aparezca el nombre
