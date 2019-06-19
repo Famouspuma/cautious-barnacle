cddc626e3a8a0dae4d7152b1d2f969a3d3dfc38d
Joaquin Berardi <joaquinberardi@gmailcom>
2019-06-19 15:23:38 -0300

    Log Out listo

--- Proyecto integrador/Modulos/js/Log.js
+++ Proyecto integrador/Modulos/js/Log.js
@@ -2,14 +2,18 @@ window.addEventListener("load", function () {
   console.log();
   if ( (window.sessionStorage.getItem('user')) != null) {
        var div = "<li class='nav-item active'>"
-          div += "<button class='btn btn-danger log'> Log out </button>"
+          div += "<button class='btn btn-danger log' > Log out </button>"
           div += "</li>"
           document.querySelector("div.log").innerHTML = div
-          document.getElementById("button.log").addEventListener("click").sessionStorage.clear();
-}
+          div = document.querySelector("div.log")
 
-
-
-
-
+          div.addEventListener("click", function() {
+            sessionStorage.clear();
+            location.reload();
+            var div = "<li class='nav-item active'>"
+                div += "<button type='button' class='btn btn-primary log' data-toggle='modal' data-target='#exampleModal' data-whatever='@mdo'>Log in </button>"
+                div += "</li>"
+                document.querySelector("div.log").innerHTML = div
+          })
+        }
 })
