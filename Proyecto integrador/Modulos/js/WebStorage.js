be9fe7ab133e9ca69244537cdba4a0a30614da8f
Marc <mdoman@udesa.edu..ar>
2019-06-18 21:00:29 -0300

    intento morboso de Log in

--- Proyecto integrador/Modulos/js/WebStorage.js
+++ Proyecto integrador/Modulos/js/WebStorage.js
@@ -1,13 +1,28 @@
 window.addEventListener("load", function () {
 
-  var baseDatos = JSON.parse(window.localStorage.getItem('user'))
-  if (baseDatos.nombre != "") {
-    UIkit.notification({
-        message: 'Bienvenido '+ baseDatos.nombre,
-        status: 'primary',
-        pos: 'bottom-center',
-        timeout: 5000
-    });  }
+
+//////////////////////////////////////////////////////////
+//   guardar_localstorage();
+//   obtener_localstorage();
+//
+//   function obtener_localstorage(){
+//
+//
+//   if ( window.localStorage.getItem('nombre')) {
+//
+//
+//
+//
+// }else {
+//   console.log("No hay entradas en el local storage");
+// }
+// }
+//
+// function guardar_localstorage(){
+
+// videooooo//////////////////////////////////////////
+
+
 
 
 var form = document.querySelector("#form")
@@ -15,56 +30,118 @@ var form = document.querySelector("#form")
 
 
 form.onsubmit= function (event) {
+      // event.preventDefault()
       var inputNombre = document.querySelector("#name")
       var inputEmail = document.querySelector("#email")
       var inputGenre = document.querySelector("#genre")
       var select = inputGenre.options[ inputGenre.selectedIndex].text
-      var emailRegex = "/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/"
 
 
-      if (inputNombre.value.length < 3) {
-        event.preventDefault();
-        UIkit.notification({
-            message: 'Ingresa un nombre de 3 caracteres o mas!',
-            status: 'warning',
-            pos: 'top-center',
-            timeout: 5000
-        });
-      }else if (inputEmail.value == "" ) {
-        event.preventDefault();
-        UIkit.notification({
-            message: 'Ingresa un email valido',
-            status: 'primary',
-            pos: 'top-center',
-            timeout: 5000
-        });
+      var name = inputNombre.value
+      var email = inputEmail.value
+      var genre = select
 
-      }else if (select == "Elegir...") {
-        event.preventDefault()
-        UIkit.notification({
-            message: 'Elegi un genero',
-            status: 'primary',
-            pos: 'bottom-center',
-            timeout: 5000
-        });
+           console.log('Hola');
 
-      }else {
-        var name = inputNombre.value
-        var email = inputEmail.value
-        var genre = select
-
-        var Usuario = {
-          nombre: name,
-          correo: email,
-          genero: genre
-        }
-
-        window.localStorage.setItem('user', JSON.stringify(Usuario))
-        console.log(window.localStorage.getItem('user'))
-        console.log(JSON.parse(window.localStorage.getItem('user')))
+      var Usuario = {
+        nombre: name,
+        correo: email,
+        genero: genre
       }
 
 
-  }
+      window.localStorage.setItem('user', JSON.stringify(Usuario))
+      console.log(window.localStorage.getItem('user'))
+      console.log(JSON.parse(window.localStorage.getItem('user')))
 
-} )
+
+}
+
+  if ( window.localStorage.getItem('Usuario.genero')=== null) {
+
+  event.preventDefault()
+  console.log("No hay entradas en el local storage");
+
+}
+
+
+})
+
+
+
+
+//////////////////////////VIEJOOOOOO///////////////////////////////////////////////////////
+
+
+
+// window.addEventListener("load", function () {
+//
+//
+//   var baseDatos = JSON.parse(window.localStorage.getItem('user'))
+//   if (baseDatos.nombre != "") {
+//     UIkit.notification({
+//         message: 'Bienvenido '+ baseDatos.nombre,
+//         status: 'primary',
+//         pos: 'top-center',
+//         timeout: 3000
+//     });  }
+//
+//
+// var form = document.querySelector("#form")
+//
+//
+//
+// form.onsubmit= function (event) {
+//       var inputNombre = document.querySelector("#name")
+//       var inputEmail = document.querySelector("#email")
+//       var inputGenre = document.querySelector("#genre")
+//       var select = inputGenre.options[ inputGenre.selectedIndex].text
+//       var emailRegex = "/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/"
+//
+//
+//       if (inputNombre.value.length < 3) {
+//         event.preventDefault();
+//         UIkit.notification({
+//             message: 'Ingresa un nombre de 3 caracteres o mas!',
+//             status: 'warning',
+//             pos: 'top-center',
+//             timeout: 3000
+//         });
+//       }else if (inputEmail.value == "" ) {
+//         event.preventDefault();
+//         UIkit.notification({
+//             message: 'Ingresa un email valido',
+//             status: 'primary',
+//             pos: 'top-center',
+//             timeout: 3000
+//         });
+//
+//       }else if (select == "Elegir...") {
+//         event.preventDefault()
+//         UIkit.notification({
+//             message: 'Elegi un genero',
+//             status: 'primary',
+//             pos: 'top-center',
+//             timeout: 3000
+//         });
+//
+//       }else {
+//         var name = inputNombre.value
+//         var email = inputEmail.value
+//         var genre = select
+//
+//         var Usuario = {
+//           nombre: name,
+//           correo: email,
+//           genero: genre
+//         }
+//
+//         window.localStorage.setItem('user', JSON.stringify(Usuario))
+//         console.log(window.localStorage.getItem('user'))
+//         console.log(JSON.parse(window.localStorage.getItem('user')))
+//       }
+//
+//
+//   }
+//
+// } )
